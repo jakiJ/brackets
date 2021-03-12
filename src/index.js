92cba9a6ce6c81098203378f10a21bf154c2cf84
julia <s98435@yandex.ru>
2021-03-12 15:03:54 +0300

    feat: decided brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let replaceStr = str;
+
+  while (str) {
+      bracketsConfig.forEach(element => { //перебираю каждый элемент массива bracketsConfig
+        str = str.replace(element.join(''), '');  //нахожу и заменяю(удалаю) пару скобок(элемент) в строке
+      });
+
+      if (str === replaceStr) {  // если они равны, значит уже совпадений на удаление не было
+        return false;
+      }
+
+      replaceStr = str; // если замена была успешная, то перезаписываю измененную строку
+  }
+
+  return true;
+
 }
